8a05a3a4982e614f6d3206be819829f74d79d597
tung <nguyenlamtung10@gmail.com>
2023-02-05 15:14:39 -0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask
-from views import views
+from flask import Blueprint, render_template, request
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+    output = request.form.to_dict()
+    t = output["t"]
+    if(t.lstrip('-+').isnumeric()):
+        t = float(t)*1.0
+    else:
+        t = 1.0
+    x = np.arange(-10, 10, 0.011)
+
+    # export image
+    plt.plot(x, np.power(x, t), 'k')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.savefig("static/image.pdf")
+    plt.savefig("static/image.png")
+    plt.close()
+
+    return render_template("index.html", t=t)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
